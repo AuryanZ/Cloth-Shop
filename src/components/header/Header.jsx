25185db2c71b406e44bb648e669d237632be48d1
Rui Zeng <ruizeng@Ruis-MacBook-Pro.local>
2023-07-30 13:44:24 +1200

    header components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-[#F6F6F7] mt-4 px-4 lg:px-32 flex justify-end items-center">
+      my cart (0)
+    </header>
+  );
+};
+
+export default Header;
